9ff86db9bdaa8daf0a42812a32b7b4a28d4405c3
TollasGyula <tollas.gyula.1143@dszcbaross.edu.hu>
2025-02-24 13:32:37 +0100

    elso

---
+++ adatok.js
@@ -0,0 +1,48 @@
+const tomb=[
+    {
+        "nev1":"Mátyás király",
+        "nev2":"Károly Róbert",
+        "nev3":"IV.Béla"
+    },
+    {
+        "nev1":"Sissi",
+        "nev2":"Frigyes császár",
+        "nev3":"Napóleon"
+    },  
+    {
+        "nev1":"Széchenyi István",
+        "nev2":"Kossuth",
+        "nev3":"József Attila"
+    }, 
+    {
+        "nev1":"Gandhi",
+        "nev2":"Nehru",
+        "nev3":"Indira Gandhi"
+    }, 
+    {
+        "nev1":"Mandela",
+        "nev2":"Desmond Tutu",
+        "nev3":"Oliver Tambo"
+    }, 
+    {
+        "nev1":"Napóleon",
+        "nev2":"Josephine",
+        "nev3":"Talleyrand"
+    }, 
+    {
+        "nev1":"Mária Terézia",
+        "nev2":"II. József",
+        "nev3":"Leopold I"
+    }, 
+    {
+        "nev1":"Kossuth Lajos",
+        "nev2":"Széchenyi",
+        "nev3":"Deák Ferenc"
+    }
+]
+const tombPoz=[
+    "Ügyes vagy","YIPIIII!!!","taveszbaktalo testvírem jau lett","DZSAAA","Viszont lágy tojás"
+]
+const tombNeg=[
+    "Te inteligentelen","Te is barossba jártál nemde?","Gyenge vagy hófehérke","Dongalábű kenukormányos salakmotoros balsors kit régen tép maradj suliba","IQ szinted a béka POPÓJA alatt"
+]
